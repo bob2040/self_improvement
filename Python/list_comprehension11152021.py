28502383cbea818541a528081f444b019fda871b
bob2040 <bob.xing@novacentrix.co.kr>
2021-11-15 16:02:39 +0800

    modify some codes in list_comprehension11152021

--- Python/list_comprehension11152021.py
+++ Python/list_comprehension11152021.py
@@ -9,7 +9,7 @@ for i in range(100):
 # [1, 2, 3, 4, 5, 6, ......, 97, 98, 99, 100]
 # [100,99,98,97]
 # [[1, 2, 3], [4, 5, 6], ......, [97, 98, 99], [100]]
-a = [x for x in zip(range(1,100),range(101, 1,-1))]
+a = [x for x in range(1, 101)]
 b = [a[x: x + 3] for x in range(0, len(a), 3)]
 print('\nExercise results:')
 print(f'a = {a}\n')
